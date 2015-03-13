90004c65d5f4762a4e7f12fd061b5a87b2797fd9
Joboto <joeisamonkey@gmail.com>
2015-03-13 23:24:44 +0000

    Adding one movement button
    Grid now appears as a JSplitPane. A default Ship (Dave) appears which
    can move North (down) at click of button.
    Want to find a way to force splits in pane to particular sizes.

--- src/Screen.java
+++ src/Screen.java
@@ -1,10 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import javax.swing.border.EmptyBorder;
-
 import java.awt.CardLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -14,9 +13,12 @@ import javax.swing.JButton;
 
 
 public class Screen extends JFrame {
-	private JPanel contentPane;
-	private JPanel buttonsPane;
+	private int size = 4;
 	private Grid grid = new Grid();
+	private Ship ship;
+	private JSplitPane splitPane;
+	private JPanel gridPane;
+	private JPanel buttonsPane;
 	private ScreenSquare squares[][];
 	private JButton actionButton;
 
@@ -31,37 +33,62 @@ public class Screen extends JFrame {
 				}
 			}
 		});
-		
 	}
 
 	public Screen() {
-		int size = 4;
-		grid.initializeGrid(size);
-		squares = new ScreenSquare[size][size];
+		splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(size, size, 0, 0));
-		for(int y = 0; y < size; y++){
-			for(int x = 0; x < size; x++){
-				squares[x][y] = new ScreenSquare(x, y, grid);
-				contentPane.add(squares[x][y]);
-			}
-		}//end for, creating grid
-		buttonsPane = new JPanel();
-		buttonsPane.add(getActionButton());
+		
+		grid.initializeGrid(size);
+		ship = new Ship(grid, "Dave");
+		grid.put(0, 0, ship);
+		
+		splitPane.setLeftComponent(getGridPane(size));
+		splitPane.setRightComponent(getButtonPane());
+		
+		JPanel wrapper = new JPanel();
+		wrapper.setLayout(new BorderLayout());
+		setContentPane(splitPane);
 		
 	}//end Screen
 	
+	private JPanel getGridPane(int size) {
+		if(gridPane == null){
+			gridPane = new JPanel();
+			gridPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			gridPane.setSize(400, 400);
+			gridPane.setLayout(new GridLayout(size, size, 0, 0));
+			squares = new ScreenSquare[size][size];
+			for(int y = 0; y < size; y++){
+				for(int x = 0; x < size; x++){
+					squares[x][y] = new ScreenSquare(x, y, grid);
+					gridPane.add(squares[x][y]);
+				}
+			}//end for, creating grid
+		}
+		return gridPane;
+	}
+	
+	private JPanel getButtonPane(){
+		if(buttonsPane == null){
+			buttonsPane = new JPanel();
+			buttonsPane.add(getActionButton(), BorderLayout.CENTER);
+		}
+		return buttonsPane;
+	}
+	
 	private JButton getActionButton() {
 		if (actionButton == null) {
-			actionButton = new JButton("Hazza!");
+			actionButton = new JButton("Northwards");
 			actionButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
-					// TODO Auto-generated method stub
-					
+					ship.move(Movement.N);
+					for(int y = 0; y < size; y++){
+						for(int x = 0; x < size; x++){
+							squares[x][y].update();
+						}
+					}
 				}
 			});
 			actionButton.setSize(10, 10);
--- src/ScreenSquare.java
+++ src/ScreenSquare.java
@@ -7,38 +7,21 @@ import javax.swing.JLabel;
 //import javax.swing.JButton;
 
 
-public class ScreenSquare extends JLabel /*implements ActionListener*/ {
-	/*int x, y;
-	byte value = 0;
-	Grid grid;*/
+public class ScreenSquare extends JLabel {
+	int x, y;
+	Grid grid;
 	
 	public ScreenSquare(int gridX, int gridY, Grid gameGrid){
-		/*x = gridX;
-		y = gridY;
-		grid = gameGrid;*/
+		
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.setText(gameGrid.whatsAt(gridX, gridY).description());
-		/*X = new ImageIcon(this.getClass().getResource("X.jpg"));
-		O = new ImageIcon(this.getClass().getResource("O.jpg"));*/
-		//this.addActionListener(this);
-	}
-	
-	/*public void actionPerformed(ActionEvent e){
-		value++;
-		value %= 3;
-		switch(value){
-		case 0:
-			this.setText(grid.whatsAt(x, x).description());
-			break;
-		case 1:
-			Ship s = new Ship(grid, "Dave");
-			grid.put(x, y, s);
-			this.setText(grid.whatsAt(x, x).description());
-			break;
-		case 2:
-			grid.whatsAt(x, y).getShip().move(Movement.N);
-			this.setText(grid.whatsAt(x, x).description());
-			break;
+		this.setSize(50, 50);
+		x = gridX;
+		y = gridY;
+		grid = gameGrid;
+		update();
 		}
-	}*/
+	
+	public void update(){
+		this.setText(grid.whatsAt(x, y).description());
+	}
 }
